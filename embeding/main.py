9f9bbff33c48ffa4841ab91cd6b61335f19e342d
OmarOsamaWT <omar.osama@tildetech.ae>
2025-12-29 14:35:57 +0200

    delete the embeding model

--- embeding/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from sentence_transformers import SentenceTransformer
-
-app = FastAPI()
-model = SentenceTransformer("BAAI/bge-base-en-v1.5") 
-
-@app.post("/embed")
-def get_embedding(text: str):
-    return {"embedding": model.encode(text).tolist()}
